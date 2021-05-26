6f088c35cfbd2b26032773f619fb56ed2ba8499c
Amogh Giri <amoghgiri01@gmail.com>
2021-05-26 01:42:45 -0400

    added socials

--- client/src/Loadout.js
+++ client/src/Loadout.js
@@ -1,29 +1,14 @@
 import React, { useEffect, useState } from "react";
 import Tabletop from "tabletop";
-import { Card } from 'semantic-ui-react';
+import { Card, Icon } from 'semantic-ui-react';
 
 function Loadout(props){
 
     var creator = ""
     var description = ""
+    var socials = ""
     var list = []
     const [data, setData] = useState({})
-
-    for(var i = 0; i < data.length; i++){
-        console.log(data[i]["Gun"], props.gun)
-        if (data[i]["Gun"].toLowerCase() === props.gun.toLowerCase()) {
-            creator = <b>{data[i]["Creator"]}</b>
-            description = <p><b>Muzzle: </b>{data[i]["Muzzle"]}<br></br><b>Barrel: </b>{data[i]["Barrel"]}
-                                <br></br><b>Underbarrel: </b>{data[i]["Underbarrel"]}<br></br><b>Laser: </b>
-                                {data[i]["Laser"]}<br></br><b>Ammunition: </b>{data[i]["Ammunition"]}<br></br>
-                                <b>Optic: </b>{data[i]["Optic"]}<br></br><b>Rear Grip: </b>{data[i]["Rear Grip"]}
-                                <br></br><b>Stock: </b>{data[i]["Stock"]}<br></br><b>Perk: </b>{data[i]["Perk"]}</p>
-            list.push(<Card>
-                        <Card.Content header = {creator} />
-                        <Card.Content description = {description} />
-                    </Card>)
-        }
-    }
 
     useEffect(() => {
         Tabletop.init({
@@ -34,6 +19,29 @@ function Loadout(props){
           console.log(data)
         });
       }, []);
+
+    for(var i = 0; i < data.length; i++){
+        console.log(data[i]["Gun"], props.gun)
+        if (data[i]["Gun"].toLowerCase() === props.gun.toLowerCase()) {
+            console.log(data[i])
+            creator = <b>{data[i]["Creator"]}</b>
+            description = <p><b>Muzzle: </b>{data[i]["Muzzle"]}<br></br><b>Barrel: </b>{data[i]["Barrel"]}
+                                <br></br><b>Underbarrel: </b>{data[i]["Underbarrel"]}<br></br><b>Laser: </b>
+                                {data[i]["Laser"]}<br></br><b>Ammunition: </b>{data[i]["Ammunition"]}<br></br>
+                                <b>Optic: </b>{data[i]["Optic"]}<br></br><b>Rear Grip: </b>{data[i]["Rear Grip"]}
+                                <br></br><b>Stock: </b>{data[i]["Stock"]}<br></br><b>Perk: </b>{data[i]["Perk"]}</p>
+            socials = <p><b>Twitch: </b>{data[i]["Twitch"]}<br></br><b>Youtube:</b> {data[i]["Youtube"]}<br></br><b>Twitter:</b> {data[i]["Twitter"]}
+                        <br></br><b>Instagram:</b> {data[i]["Instagram"]}</p>
+                        
+            list.push(<Card>
+                        <Card.Content header = {creator} />
+                        <Card.Content description = {description} />
+                        <Card.Content extra>
+                            {socials}
+                        </Card.Content>
+                    </Card>)
+        }
+    }
 
     return (
         <div className="loadout">
